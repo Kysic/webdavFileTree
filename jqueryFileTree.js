d623ea7faf1820d877beb333d6f3ed53b7bf50cb
Kysic <ludocourrier-github@yahoo.fr>
2016-01-11 18:52:55 +0100

    Replace (and move) use of escape() by encodeURI()

--- jqueryFileTree.js
+++ jqueryFileTree.js
@@ -51,20 +51,20 @@ if(jQuery) (function($){
 			if( o.multiFolder == undefined ) o.multiFolder = true;
 			if( o.loadMessage == undefined ) o.loadMessage = 'Loading...';
 			if( o.ajaxConnector == undefined ) {
-			    o.ajaxConnector = function(t, connectorCallback) {
-					$.post(o.script, { dir: t }, connectorCallback);
+			    o.ajaxConnector = function(dir, connectorCallback) {
+					$.post(o.script, { dir: encodeURI(dir) }, connectorCallback);
 			    };
 			}
 			
 			$(this).each( function() {
 
-				function showTree(c, t) {
+				function showTree(c, dir) {
 					$(c).addClass('wait');
 					$(".jqueryFileTree.start").remove();
-					o.ajaxConnector(t, function(data) {
+					o.ajaxConnector(dir, function(data) {
 						$(c).find('.start').html('');
 						$(c).removeClass('wait').append(data);
-						if( o.root == t ) $(c).find('UL:hidden').show();
+						if( o.root == dir ) $(c).find('UL:hidden').show();
 						else $(c).find('UL:hidden').slideDown({ duration: o.expandSpeed, easing: o.expandEasing });
 						bindTree(c);
 					});
@@ -80,7 +80,7 @@ if(jQuery) (function($){
 									$(this).parent().parent().find('LI.directory').removeClass('expanded').addClass('collapsed');
 								}
 								$(this).parent().find('UL').remove(); // cleanup
-								showTree( $(this).parent(), escape($(this).attr('rel').match( /.*\// )) );
+								showTree( $(this).parent(), $(this).attr('rel').match( /.*\// ) );
 								$(this).parent().removeClass('collapsed').addClass('expanded');
 							} else {
 								// Collapse
